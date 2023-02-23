882b32ade49e73c8af77d5449d896132d1ee1c5b
Friturito <ricardoxgonzalez7427809@gmail.com>
2023-02-23 21:32:37 +0000

    8. Renderizado de Elementos

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Componente from "./components/Componente";
 import Propiedades from "./components/Propiedades";
 import Estado from "./components/Estado";
 import RenderizadoCondicional from "./components/RenderizadoCondicional";
+import RenderizadoElementos from "./components/RenderizadoElementos";
 
 function App() {
   let nombre = "Ricardo";
@@ -54,6 +55,8 @@ function App() {
           <Estado />
           <hr />
           <RenderizadoCondicional />
+          <hr />
+          <RenderizadoElementos />
         </section>
       </header>
     </div>
