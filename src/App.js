19317d0962dcb3c3ba8a2de28293a415d31070e7
Friturito <ricardoxgonzalez7427809@gmail.com>
2023-02-26 20:39:20 +0000

    9. Eventos & Binding

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Propiedades from "./components/Propiedades";
 import Estado from "./components/Estado";
 import RenderizadoCondicional from "./components/RenderizadoCondicional";
 import RenderizadoElementos from "./components/RenderizadoElementos";
+import Eventos from "./components/Eventos";
 
 function App() {
   let nombre = "Ricardo";
@@ -57,6 +58,8 @@ function App() {
           <RenderizadoCondicional />
           <hr />
           <RenderizadoElementos />
+          <hr />
+          <Eventos />
         </section>
       </header>
     </div>
