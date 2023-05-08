120c4e0c5f76b2124a108ea34f8592483754c22c
Ricardo <ricardoxgonzalez7427809@gmail.com>
2023-05-07 20:06:00 -0400

    Frameworks CSS + Boostrap & Bulma

--- react-framework-css/src/App.js
+++ react-framework-css/src/App.js
@@ -1,6 +1,14 @@
-import Boostrap from "./Components/Boostrap";
+/*import Boostrap from "./Components/Boostrap";*/
+import Bulma from "./Components/bulma";
 
 function App() {
-  return (
-    <div>
-      <h1>Frameworks CSS con Reac
+    return (
+        <div>
+            <h1>Frameworks CSS con React</h1>
+            {/*<Boostrap/>*/}
+            <Bulma />
+        </div>
+    );
+}
+
+export default App;
